68eb43722d94eb0a68d3927afbb1aa664e8ec7b8
Ramoncito29 <97406586+Ramoncito29@users.noreply.github.com>
2024-02-29 21:40:57 -0300

    se agregaron las 3 lineas de autos

--- main.cpp
+++ main.cpp
@@ -6,17 +6,15 @@
 using namespace std;
 
 
-// para dibujar, primero tengo que borrar la posicion actual
-// acualizar la coodenada
-// y finalmente volver a dibujar
 
+
+	
 void dibujar(int x, int y, int z){
 	
 	putchxy(x,y,z);
 	
 }
-	
-		
+			
 	void borrar(int x, int y){
 		
 		// probar comentar
@@ -26,6 +24,7 @@ void dibujar(int x, int y, int z){
 		
 		int main(int argc, char *argv[]) {
 			
+			
 			cout<<"Con las teclas se mueve (z sale)"<<endl;
 			
 			int x=60;
@@ -33,11 +32,26 @@ void dibujar(int x, int y, int z){
 			int rana = 'x';
 			putchxy(x,y,rana); 
 			
-			int vx = 12;
-			int vy = 28;
+			int vx1 = 28;
+			int vy1 = 28;
 			int coche = 'y';
-			putchxy(vx,vy,coche);
+			putchxy(vx1,vy1,coche);
 			
+			int vx2 = 56;
+					
+			int vx3 = 84;
+			
+			int vx4 = 84;
+				
+			int vx5 = 56;
+			
+			int vx6 = 28;
+			
+			int vy2 = 20;
+		
+			
+			putchxy(vx2,vy1,coche);
+			putchxy(vx3,vy1,coche);
 			
 			int tiempo = clock();
 			int velocidad =60;
@@ -45,20 +59,75 @@ void dibujar(int x, int y, int z){
 			
 			
 			
+//			
+			
 			
 			
 			while(true){
-				
+				//primera linea
 				if((paso + tiempo) < clock()){
-					if(vx == 96){
-						vx = 12;
+					if(vx1 == 96){
+						vx1 = 12;
 					};
 					
-					putchxy(vx,vy, ' ');
-					vx = vx + 4;
-					putchxy(vx,vy,coche);
+					putchxy(vx1,vy1, ' ');
+					vx1 = vx1 + 4;
+					putchxy(vx1,vy1, coche);
+					tiempo = clock();
+					
+					if(vx2 == 96){
+						vx2 = 12;
+					};
+					
+					putchxy(vx2,vy1, ' ');
+					vx2 = vx2 + 4;
+					putchxy(vx2,vy1, coche);
+					tiempo = clock();
+					
+					if(vx3 == 96){
+						vx3 = 12;
+					};
+					
+					putchxy(vx3,vy1, ' ');
+					vx3 = vx3 + 4;
+					putchxy(vx3,vy1, coche);
+					tiempo = clock();
+					//segunda linea
+					
+					if(vx4 == 12){
+						vx4 = 96;
+					};
+					
+					putchxy(vx4,vy2, ' ');
+					vx4 = vx4 - 4;
+					putchxy(vx4,vy2, coche);
+					tiempo = clock();
+////					
+					if(vx5 == 12){
+						vx5 = 96;
+					};
+					
+					putchxy(vx5,vy2, ' ');
+					vx5 = vx5 - 4;
+					putchxy(vx5,vy2, coche);
+					tiempo = clock();
+					
+					if(vx6 == 12){
+						vx6 = 96;
+					};
+					
+					putchxy(vx6,vy2, ' ');
+					vx6 = vx6 - 4;
+					putchxy(vx6,vy2, coche);
 					tiempo = clock();
 				};
+				
+				
+			
+				
+				
+				
+				
 				
 				if(kbhit()) { 
 					int tecla=getch();
@@ -105,9 +174,8 @@ void dibujar(int x, int y, int z){
 						y = y + 8;
 						dibujar(x,y,rana);
 						};
-						
 						break;
-					case 'z':
+						
 						return 1;
 					}
 					
@@ -116,7 +184,6 @@ void dibujar(int x, int y, int z){
 				
 			}
 			
-				
 					
 						
 			return 0;
